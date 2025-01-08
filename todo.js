eb7828eb46b9b7ff0a7e5ef013e1fcc8ab8e3aa8
AshishIT13 <ay87896@gmail.com>
2025-01-08 21:22:21 +0530

    Updated

--- todo.js
+++ todo.js
@@ -1,94 +1,96 @@
-const fs=require("fs").promises;
-const path=require("path");
-const todoFile="Todo.txt";
-async function main(){
-    const task=process.argv[2];
-    const msg=process.argv.slice(3).join(' ');
-    if(!task){
-        console.log("Please provide a task type (add, list, delete).");
+const fs = require("fs");
+const path = require("path");
+const todoFile = "Todo.txt";
+
+function main() {
+    const task = process.argv[2];
+    const msg = process.argv.slice(3).join(' ');
+
+    if (!task) {
+        console.log("Please provide a task type (add, list, delete).\n");
         return;
     }
-    switch(task){
+
+    switch (task) {
         case "add":
-            if(!msg){
-                console.log("Please provide a task to add.");
+            if (!msg) {
+                console.log("Please provide a task to add.\n");
                 return;
             }
-            await addToDo(msg);
-            console.log("Todo added!");
+            addToDo(msg);
+            console.log("Todo added!\n");
             break;
         case "list":
-            await listTodos();
+            listTodos();
             break;
         case "delete":
-            const index=parseInt(msg, 10);
-            if(isNaN(index)){
-                console.log("Please provide a valid task number to delete.");
+            const index = parseInt(msg, 10);
+            if (isNaN(index)) {
+                console.log("Please provide a valid task number to delete.\n");
                 return;
             }
-            const wasDeleted=await deleteTodo(index);
-            if(wasDeleted){
-                console.log("Todo deleted!");
-            } 
-            else{
-                console.log("Invalid task number.");
+            const wasDeleted = deleteTodo(index);
+            if (wasDeleted) {
+                console.log("Todo deleted!\n");
+            } else {
+                console.log("Invalid task number.\n");
             }
             break;
         default:
-            console.log("Unknown task. Available tasks: add, list, delete.");
+            console.log("Unknown task. Available tasks: add, list, delete.\n");
             break;
     }
 }
-async function addToDo(msg){
-    try{
-        await fs.appendFile(todoFile,`${msg}\n`);
-    } 
-    catch(err){
-        console.error("Error adding task:",err);
+
+function addToDo(msg) {
+    try {
+        fs.appendFileSync(todoFile, `${msg}\n`);
+    } catch (err) {
+        console.error("Error adding task:", err);
     }
 }
-async function listTodos(){
-    try{
-        const todos=await getTodos();
-        if (todos.length===0){
-            console.log("No todos found.");
+
+function listTodos() {
+    try {
+        const todos = getTodos();
+        if (todos.length === 0) {
+            console.log("No todos found.\n");
             return;
         }
-        todos.forEach((task,index)=>{
+        todos.forEach((task, index) => {
             console.log(`${index + 1}. ${task}`);
         });
-    } 
-    catch(err){
-        console.error("Error listing tasks:",err);
+    } catch (err) {
+        console.error("Error listing tasks:", err);
     }
 }
-async function deleteTodo(index){
-    try{
-        const todos=await getTodos();
-        if(index< 1||index>todos.length){
+
+function deleteTodo(index) {
+    try {
+        const todos = getTodos();
+        if (index < 1 || index > todos.length) {
             return false;
         }
-        todos.splice(index-1,1);
-        if(todos.length>0){
-            await fs.writeFile(todoFile,todos.join("\n")+"\n");
-        } 
-        else{
-            await fs.writeFile(todoFile,'');
+        todos.splice(index - 1, 1);
+        if (todos.length > 0) {
+            fs.writeFileSync(todoFile, todos.join("\n") + "\n");
+        } else {
+            fs.writeFileSync(todoFile, '');
         }
         return true;
-    } 
-    catch(err){
-        console.error("Error deleting task:",err);
+    } catch (err) {
+        console.error("Error deleting task:", err);
         return false;
     }
 }
-async function getTodos(){
-    try{
-        const data=await fs.readFile(todoFile,"utf-8");
+
+function getTodos() {
+    try {
+        const data = fs.readFileSync(todoFile, "utf-8");
         return data.trim().split("\n").filter(Boolean);
-    } 
-    catch (err){
+    } catch (err) {
         return [];
     }
 }
+
 main();
